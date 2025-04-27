5657a24e875b20a97768ab0f0189ea5b40a7cd2c
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-04-27 08:55:56 +0300

    please

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();  
-const router = jsonServer.router('db.json');  
-const middlewares = jsonServer.defaults();  
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares);  
+server.use(middlewares);
 
 server.use('/api', router);  
 
-const PORT = process.env.PORT || 10000;  
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
